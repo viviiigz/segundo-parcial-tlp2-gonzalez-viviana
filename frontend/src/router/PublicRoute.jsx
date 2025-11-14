eb6dba492233c1a3b2646960c06f6b55133d7538
Viviana Gonzalez <vivigz06@gmail.com>
2025-11-14 14:53:02 -0300

    se instalaron las dependencias en la carpeta del front

--- frontend/src/router/PublicRoute.jsx
+++ frontend/src/router/PublicRoute.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router";
-import Loading from "../components/Loading";
+import { Loading } from "../components/Loading.jsx";
 
 export const PublicRoute = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
